575ef0365c70a84638ddbe26714332035d1f7be5
1808T <93326398+1808T@users.noreply.github.com>
2022-01-06 21:14:32 +0700

    final

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
-let array1 = ["BINH", "HUNG", "PHUOC",];
-let result1 = [];
-for (let index = 0; index < array1.length; index++) {
-    if (array1[i].length > array1[i+1].length) {
-        let temp = array1[i+1];
-        array1[i] = array1[i+1];
-        array1[i+1] = temp;
-    }
-}
-console.log(array1);
+// Bài 1
+// let array1 = ["BINH", "HUNG", "PHUOC", "CAO", "KHANH"];
+// let eleLength = 0;
+// let longest = "";
+// let result1 = [];
+// for (let i = 0; i < array1.length; i++) {
+//     if (array1[i].length > eleLength) {
+//         eleLength = array1[i].length;
+//         longest = array1[i];
+//     }
+// }
+// result1.push(longest);
+// console.log(result1);
+
+// Bài 2
+// let arrayInput = [60, 40, 55, 75, 64]
+// let result2 = [];
+// let sum1 = 0;
+// let sum2 = 0;
+// for (let n = 0; n < arrayInput.length; n+=2) {
+//         sum1 += arrayInput[n];
+// }
+// console.log(sum1);
+// for (let m = 1; m < arrayInput.length; m+=2) {
+//     sum2 += arrayInput[m];
+// }
+// console.log(sum2);
+// result2.push(sum1);
+// result2.push(sum2);
+// console.log(result2);
+
+
+// // Bài 3
+const randomBtn = document.querySelector("#random")
+const background = document.querySelector("body")
+const p = document.querySelector("p")
+randomBtn.addEventListener("click", function() {
+    let text = "#";
+    let character = "ABCDEF0123456789";
+    for (let i = 0; i < 6; i++) 
+        text += character.charAt(Math.floor(Math.random() * character.length));
+    console.log(text);
+    background.style.background = `${text}`;
+    p.innerHTML = `${text}`;
+    const copyBtn = document.createElement("button");
+    p.appendChild(copyBtn);
+    copyBtn.innerHTML = "Copy"
+    copyBtn.addEventListener("click", function() {
+        p.select();
+        navigator.clipboard.writeText(p);
+    })
+});
