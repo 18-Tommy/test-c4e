b0f1f47a7e1152b2da12c704b82cd67d37ef1756
1808T <93326398+1808T@users.noreply.github.com>
2022-01-06 21:31:41 +0700

    final

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Bài 1
-let array1 = ["BINH", "HUNG", "PHUOC", "CAO", "KHANH"];
-let eleLength = 0;
-let longest = "";
-let result1 = [];
-for (let i = 0; i < array1.length; i++) {
-    if (array1[i].length > eleLength) {
-        eleLength = array1[i].length;
-        longest = array1[i];
-    }
-}
-for (let a = 0; a < array1.length; a++) {
-    if (array1[a].length == longest.length) {
-        result1.push(array1[a]);
-    }
-}
-console.log(result1);
+// let array1 = ["BINH", "HUNG", "PHUOC", "CAO", "KHANH"];
+// let eleLength = 0;
+// let longest = "";
+// let result1 = [];
+// for (let i = 0; i < array1.length; i++) {
+//     if (array1[i].length > eleLength) {
+//         eleLength = array1[i].length;
+//         longest = array1[i];
+//     }
+// }
+// for (let a = 0; a < array1.length; a++) {
+//     if (array1[a].length == longest.length) {
+//         result1.push(array1[a]);
+//     }
+// }
+// console.log(result1);
 
 // Bài 2
 // let arrayInput = [60, 40, 55, 75, 64]
@@ -34,23 +34,23 @@ console.log(result1);
 // console.log(result2);
 
 
-// // // Bài 3
-// const randomBtn = document.querySelector("#random")
-// const background = document.querySelector("body")
-// const p = document.querySelector("p")
-// randomBtn.addEventListener("click", function() {
-//     let text = "#";
-//     let character = "ABCDEF0123456789";
-//     for (let i = 0; i < 6; i++) 
-//         text += character.charAt(Math.floor(Math.random() * character.length));
-//     console.log(text);
-//     background.style.background = `${text}`;
-//     p.innerHTML = `${text}`;
-//     const copyBtn = document.createElement("button");
-//     p.appendChild(copyBtn);
-//     copyBtn.innerHTML = "Copy"
-//     copyBtn.addEventListener("click", function() {
-//         p.select();
-//         navigator.clipboard.writeText(p);
-//     })
-// });
+// Bài 3
+const randomBtn = document.querySelector("#random")
+const background = document.querySelector("body")
+const p = document.querySelector("p")
+randomBtn.addEventListener("click", function() {
+    let text = "#";
+    let character = "ABCDEF0123456789";
+    for (let i = 0; i < 6; i++) 
+        text += character.charAt(Math.floor(Math.random() * character.length));
+    console.log(text);
+    background.style.background = `${text}`;
+    p.innerHTML = `${text}`;
+    const copyBtn = document.createElement("button");
+    p.appendChild(copyBtn);
+    copyBtn.innerHTML = "Copy"
+    copyBtn.addEventListener("click", function() {
+        p.select();
+        navigator.clipboard.writeText(p);
+    })
+});
